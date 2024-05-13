1c091f48618debef5765df96b1bdd92e7c8fc12e
Dağhan Ünal <daghanunal20@gmail.com>
2024-05-13 20:56:16 +0300

    Add search functionality and update method bug

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,18 @@
 const users = []; // This would ideally be a database in a real application
 
 exports.listUsers = (req, res) => {
+    const { search } = req.query;
+    if (search) {
+        const filteredUsers = users.filter(u => u.name.toLowerCase().includes(search.toLowerCase()));
+        res.json(filteredUsers);
+        return;
+    }
     res.json(users);
 };
 
 exports.createUser = (req, res) => {
     const user = req.body;
+    user.id = users.length + 1; // Assign a unique ID
     users.push(user);
     res.status(201).send(user);
 };
@@ -23,6 +30,7 @@ exports.updateUser = (req, res) => {
     if (!user) {
         return res.status(404).send('User not found');
     }
+    // Bug: Does not validate if the incoming request body is valid
     Object.assign(user, req.body);
     res.send(user);
 };
